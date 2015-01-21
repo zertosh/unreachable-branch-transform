51dcf89e47d219e2d0766ee912132a1d2bd82aba
Asa Ayers <Asa@AsaAyers.com>
2015-01-20 22:58:44 -0500

    Forgot to include the dot prefix

--- index.js
+++ index.js
@@ -8,7 +8,12 @@ var visitorList = require('./unreachableBranchVisitors').visitorList;
 
 function process(file, opts) {
   // pass exts to whitelist a set of extensions to process.
-  var exts = [].concat(opts.exts || [])
+  var exts = [].concat(opts.exts || []).map(function(ext) {
+      // Make sure all extensions start with a . because path.extname(file)
+      // includes the .
+      if (ext.charAt(0) === '.') return ext
+      return '.' + ext
+  })
 
   if (reJSONFile.test(file)) return through();
   if (exts.length && exts.indexOf(path.extname(file)) === -1) return through();
